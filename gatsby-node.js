5a99c1a4e7c8b1479c5e09165d26cee270ea02e5
Florian Gächter <florian.gaechter@hey.com>
2021-01-28 13:39:28 +0100

    Initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,53 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-// You can delete this file if you're not using it
+const fetch = require(`node-fetch`)
+const base64 = require(`base-64`)
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+const APP_NODE_TYPE = `OpenChannelApps`
+
+exports.onPreInit = () => console.log('Loaded gatsby-source-openchannel');
+
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+  getNodesByType,
+}, {
+    APIBaseURL, MarketplaceId, Secret
+}) => {
+  const { createNode } = actions
+
+  if (!APIBaseURL) {
+      throw new Error(`APIBaseURL is required`)
+  }
+
+  if (!MarketplaceId) {
+      throw new Error(`MarketplaceId is required`)
+  }
+
+  if (!Secret) {
+      throw new Error(`Secret is required`)
+  }
+
+  const response = await fetch(`${APIBaseURL}/apps`, {
+    headers: {
+      Authorization: `Basic ${base64.encode(`${MarketplaceId}:${Secret}`)}`
+    }
+  })
+
+  const data = await response.json()
+
+  data.list.forEach(app =>
+    createNode({
+      ...app,
+      id: createNodeId(`${APP_NODE_TYPE}-${app.appId}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: APP_NODE_TYPE,
+        content: JSON.stringify(app),
+        contentDigest: createContentDigest(app),
+      },
+    })
+  )
+
+  return
+}
